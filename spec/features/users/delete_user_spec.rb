36d7358b82e056b998352b596f5c4196861b3ad7
Robert Farese <robertfarese@yahoo.com>
2016-08-22 15:09:57 -0400

    added tests and functionality for deleting a user account

---
+++ spec/features/users/delete_user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature "User deletes account", type: :feature do
+
+  scenario "unauthenticated user can't access the edit profile page to delete an account" do
+    visit root_path
+
+    expect(page).to_not have_content("Edit Profile")
+  end
+
+  scenario "authenticated user successfully deletes their account" do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    click_link "Edit Profile"
+
+    click_link "Cancel my account"
+
+
+    expect(page).to have_content("Bye! Your account has been successfully cancelled. We hope to see you again soon.")
+    expect(User.all.size).to eq(0)
+  end
+end
