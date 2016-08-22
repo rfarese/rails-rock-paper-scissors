a6f02e1c90c3d0a6d7803749e5efe087f6846f13
Robert Farese <robertfarese@yahoo.com>
2016-08-22 15:07:01 -0400

    added tests and functionality for editing a user profile

---
+++ spec/features/users/user_edits_profile_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.feature "User updates profile", type: :feature do
+  let(:user) { FactoryGirl.create(:user) }
+
+  def sign_in_and_navigate
+    sign_in(user)
+    click_link "Edit Profile"
+  end
+
+  def fill_in_new_password
+    fill_in "user_password", with: "passwording"
+    fill_in "Password confirmation", with: "passwording"
+  end
+
+  scenario "unauthenticated user doesn't view edit profile link" do
+    visit root_path
+
+    expect(page).to_not have_content("Edit Profile")
+  end
+
+  scenario "authenticated user successfully edits their profile" do
+    sign_in_and_navigate
+    fill_in "Email", with: 'newUser@example.com'
+    fill_in_new_password
+    fill_in "Current password", with: "password"
+    click_button "Update"
+
+    expect(page).to have_content("Your account has been updated successfully")
+  end
+
+  scenario "authenticated user adds invalid information" do
+    sign_in_and_navigate
+    fill_in "Email", with: ''
+    fill_in_new_password
+    fill_in "Current password", with: "passwording"
+    click_button "Update"
+
+    expect(page).to have_content("Please review the problems below:")
+    expect(page).to have_content("can't be blank")
+    expect(page).to have_content("is invalid")
+    expect(page).to_not have_content("Your account has been updated successfully")
+  end
+end
