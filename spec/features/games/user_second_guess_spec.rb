303e1f2cc0f59e3c945b29aec5ef88e75f9d7b6d
Robert Farese <robertfarese@yahoo.com>
2016-08-23 09:19:32 -0400

    added tests for user creating 2 guesses

---
+++ spec/features/games/user_second_guess_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.feature "User creates first guess;", type: :feature do
+  let(:user) { FactoryGirl.create(:user) }
+
+  scenario "user chooses 'rock', then 'paper'" do
+    sign_in(user)
+    click_link "New Game"
+    click_link "Rock"
+    updated_user = User.all.first
+
+    expect(updated_user.guess).to eq("rock")
+
+    click_link "Paper"
+    new_updated_user = User.all.first
+
+    expect(new_updated_user.guess).to eq("paper")
+  end
+
+  scenario "user chooses 'paper', then chooses 'rock'" do
+    sign_in(user)
+    click_link "New Game"
+    click_link "Paper"
+    updated_user = User.all.first
+
+    expect(updated_user.guess).to eq("paper")
+
+    click_link "Rock"
+    new_updated_user = User.all.first
+
+    expect(new_updated_user.guess).to eq("rock")
+  end
+
+  scenario "user chooses 'scissors', then chooses 'paper'" do
+    sign_in(user)
+    click_link "New Game"
+    click_link "Scissors"
+    updated_user = User.all.first
+
+    expect(updated_user.guess).to eq("scissors")
+
+    click_link "Paper"
+    new_updated_user = User.all.first
+
+    expect(new_updated_user.guess).to eq("paper")
+  end
+
+  scenario "user chooses 'scissors', then chooses 'rock'" do
+    sign_in(user)
+    click_link "New Game"
+    click_link "Scissors"
+    updated_user = User.all.first
+
+    expect(updated_user.guess).to eq("scissors")
+
+    click_link "Rock"
+    new_updated_user = User.all.first
+
+    expect(new_updated_user.guess).to eq("rock")
+  end
+end
